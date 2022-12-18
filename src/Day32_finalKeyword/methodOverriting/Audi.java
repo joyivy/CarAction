22570bd0fbf91f7b601f397f3aa1d27be1ad83f6
satarovic <fsata86@gmail.com>
2022-12-18 14:56:00 -0500

    car abject

---
+++ src/Day32_finalKeyword/methodOverriting/Audi.java
@@ -0,0 +1,13 @@
+package Day32_finalKeyword.methodOverriting;
+
+public class Audi extends Car{
+
+    public Audi(String model, String color, int year, double price) {
+        super(model, color, year, price);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Press the start botton to start. "+ getModel());
+    }
+}
