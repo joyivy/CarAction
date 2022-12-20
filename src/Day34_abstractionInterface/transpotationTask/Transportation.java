139551568d1a6105ba65affe037af60c58b10aa3
satarovic <fsata86@gmail.com>
2022-12-20 10:05:31 -0500

    devam

--- src/Day34_abstractionInterface/transpotationTask/Transportation.java
+++ src/Day34_abstractionInterface/transpotationTask/Transportation.java
@@ -1,4 +1,45 @@
 package Day34_abstractionInterface.transpotationTask;
 
 public class Transportation {
+
+    private  final String make, model;
+    private String color;
+    private  final int year;
+    private double price;
+
+    public Transportation(String make, String model, String color, int year, double price) {
+        this.make = make;
+        this.model = model;
+        setColor(color);
+        this.year = year;
+        setPrice(price);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
