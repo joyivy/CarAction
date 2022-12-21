9c969b72c63bcb61e5a1e2715c84cb643e3681b2
satarovic <fsata86@gmail.com>
2022-12-21 15:41:53 -0500

    Day36

---
+++ src/Day36_Polymorphism/ReferenceTypeCastings.java
@@ -0,0 +1,26 @@
+package Day36_Polymorphism;
+
+import Day29_inhertitance.animalTask.Animal;
+import Day29_inhertitance.animalTask.Cat;
+import Day29_inhertitance.animalTask.Dog;
+
+public class ReferenceTypeCastings {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+       // Cat cat = new Dog();
+
+        Animal animal =(Animal) new Dog(); // implicit casting
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++");
+        Animal animal1=new Dog();
+        animal1.setInfo("Max","Husky", 'F', 25,"Midium","Brown");
+        animal1.eat();
+        animal1.drink();
+        animal1.sleep();
+       // ((Dog)animal1).bark(); one time use only
+        Dog dog1 = (Dog) animal1; //useing multiple times
+        dog1.bark();
+    }
+
+
+}
