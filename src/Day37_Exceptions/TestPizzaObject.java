bb24f77cd67fb155570f09853d7127a2e3c14c0a
satarovic <fsata86@gmail.com>
2022-12-22 13:28:10 -0500

    Day37

---
+++ src/Day37_Exceptions/TestPizzaObject.java
@@ -0,0 +1,15 @@
+package Day37_Exceptions;
+
+public class TestPizzaObject {
+    public static void main(String[] args) {
+
+
+        Pizza pizza1= new Pizza('S',2,3);
+        Pizza pizza2= new Pizza('S',2,3);
+
+        System.out.println(pizza1==pizza2);
+
+        System.out.println(pizza1.equals(pizza2));
+    }
+}
+
