6a8cfcdb31337ebd4ae0e77810a7b2eaeed02825
satarovic <fsata86@gmail.com>
2022-12-22 14:05:36 -0500

    Day37

--- src/Day37_Exceptions/ExceptionsIntro.java
+++ src/Day37_Exceptions/ExceptionsIntro.java
@@ -1,5 +1,7 @@
 package Day37_Exceptions;
 
+import java.io.FileInputStream;
+
 public class ExceptionsIntro {
     public static void main(String[] args) {
         String str = "Java";
@@ -9,10 +11,25 @@ public class ExceptionsIntro {
         Pizza pizza= null;
        // pizza.calcCost();// unhecked exceptions
 
-        System.out.println(50/0); // unhecked exceptions
+       // System.out.println(50/0);  unhecked exceptions
 
         System.out.println("Hello World");
 
+        System.out.println("============================================");
+
+
+        int score = 100;
+
+        if (score >59){
+            System.out.println("your grade is D");
+        }else if(score>70){
+            System.out.println("Your grade is C");
+        }
+        System.out.println("__________________________________________________");
+
+       // FileInputStream file = new FileInputStream(""); checked exceptions
+
+        //Thread.sleep(3000);  checked exceptions.
 
 
     }
