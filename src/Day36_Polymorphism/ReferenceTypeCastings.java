2485332fca02bfb1d2c0f917d4b494eba8362947
satarovic <fsata86@gmail.com>
2022-12-23 11:03:32 -0500

    Day36

--- src/Day36_Polymorphism/ReferenceTypeCastings.java
+++ src/Day36_Polymorphism/ReferenceTypeCastings.java
@@ -3,10 +3,10 @@ package Day36_Polymorphism;
 import Day29_inhertitance.animalTask.Animal;
 import Day29_inhertitance.animalTask.Cat;
 import Day29_inhertitance.animalTask.Dog;
-import Day33_abstraction.EmployeeTask.Developer;
-import Day33_abstraction.EmployeeTask.Driver;
-import Day33_abstraction.EmployeeTask.Employee;
-import Day33_abstraction.EmployeeTask.Tester;
+import Day33_abstraction.EmployeeTask.*;
+import Day35_polymorphism.transpotationTask.AutoPilot;
+import Day35_polymorphism.transpotationTask.Electric;
+import Day35_polymorphism.transpotationTask.Tesla;
 import day30_inheritance.phoneTask.Nokia;
 import day30_inheritance.phoneTask.Phone;
 
@@ -46,7 +46,27 @@ public class ReferenceTypeCastings {
 
          Driver driver = (Driver) employee;
 
+        System.out.println("__________________________________");
+        Electric electric= new Tesla("Tesla","Model Y","Blue",2020,55000);
+        electric.charge();
+        ((Tesla)electric).selfdrive();
+        ( (AutoPilot)electric ).selfdrive();
 
+        System.out.println("-------------------------------------------------------------");
+
+        Employee employee1 = new teacher("James", 45,  'M', "B1", "Math Teacher", 75000);
+
+        Employee employee2 = new Developer("Lucy", 30, 'F', "C1", "Java Developer", 95000, "Java");
+
+        Employee employee3 = new Driver("Aaron", 48, 'M', "D1", "Truck Driver", 90000);
+
+        Employee employee4 = new Tester("Emily", 35, 'F', "E1", "Manual Tester", 8000);
+
+
+        employee1.work();
+        employee2.work();
+        employee3.work();
+        employee4.work();
 
 
         //    ( (IPhone)phone ).faceTime(123456); // Nokia can not be converted to Iphone, because there is not "IS A" relationship between Nokia and Iphone
